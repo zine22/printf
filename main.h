94196bb0e8f8d623b0d323b66cd6015701117355
zine22 <kamiljamil15@gmail.com>
2023-07-25 05:14:55 -0700

    printf

--- main.h
+++ main.h
@@ -45,6 +45,7 @@ unsigned int h_modifier   : 1;
 unsigned int l_modifier   : 1;
 } params_t;
 
+
 /**
 * struct spec - struct token
 *
@@ -61,8 +62,42 @@ int (*f)(va_list, params_t *);
 int print_percent(va_list a, params_t *params);
 int print_string(va_list a, params_t *params);
 int print_char(va_list a, params_t *params);
-
+int print_S(va_list a, params_t *params);
 int print_int(va_list a, params_t *params);
+
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list a, params_t *params);
+int print_address(va_list a, params_t *params);
+
+
+int (*get_spec(char *s))(va_list a, params_t *params);
+int get_print_func(char *s, va_list a, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+char *get_width(char *s, va_list a, params_t *params);
+
+
+int print_hex(va_list a, params_t *params);
+int print_HEX(va_list a, params_t *params);
+int print_binary(va_list a, params_t *params);
+int print_octal(va_list a, params_t *params);
+
+
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list a, params_t *params);
+int print_rot13(va_list a, params_t *params);
+
+
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+
+
+void init_params(params_t *params, va_list a);
+
+char *get_precision(char *p, params_t *params, va_list a);
 
 int _puts(char *str);
 int _putchar(int c);
